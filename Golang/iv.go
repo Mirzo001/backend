8d8255acd42933e0208d79c576261ed5777385c2
Mirzo <xmirzo001@gmail.com>
2022-05-18 23:40:57 +0500

    done iv.go

---
+++ Golang/iv.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(MySquareRoot(10, 2))
+}
+
+// func MySquareRoot(num, precision uint) (result float64) {
+// 	// DO NOT USE math.Sqrt!
+
+// 	//
+// 	// WRITE YOUR CODE HERE
+// 	//
+
+// 	return
+// }
+
+func MySquareRoot(number int, precision uint) float64 {
+	// var start int = 0
+	// var end int = number
+	var mid int
+
+	// variable to store the result
+	var result float64
+
+	// for computing integral part
+	// of square root of number
+	for start, end := int(0), number; start <= end; {
+		mid = (start + end) / 2
+		if mid*mid == number {
+			result = float64(mid)
+			break
+		}
+
+		// incrementing start if integral
+		// part lies on right side of the mid
+		if mid*mid < number {
+			start = mid + 1
+			result = float64(mid)
+		} else {
+			end = mid - 1
+		}
+	}
+
+	// For computing the fractional part
+	// of square root upto given precision
+	var increment float64 = 0.1
+	for i := uint(0); i < precision; i++ {
+		// while (result * result <= number) {
+		// 	result += increment;
+		// }
+		for {
+			result += increment
+			if result*result > float64(number) {
+				break
+			}
+
+		}
+
+		// loop terminates when result * result > number
+		result = result - increment
+		increment = increment / 10
+	}
+	return result
+}
