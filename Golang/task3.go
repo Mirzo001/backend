822a881af68d4283fa010db26b758dfc2dbb0586
Mirzo <xmirzo001@gmail.com>
2022-05-21 14:42:00 +0500

    done task3 in go

--- Golang/task3.go
+++ Golang/task3.go
@@ -2,20 +2,19 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
 	var r float32 = 10.04
 
-	// fmt.Scanf("%f", &r)
 	fmt.Println("R =", r)
 
 	fmt.Printf("Area: %0.2f\n", area(r))
 }
 
 func area(r float32) (area float32) {
-	//
-	// WRITE YOUR CODE HERE
-	//
+	area = 4*r*r - math.Pi*r*r
 	return area
+
 }
